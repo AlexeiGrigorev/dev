06ef9cc151a8550cb80dc556e5125a7e87238ca3
AlexeiGrigorev <115982267+AlexeiGrigorev@users.noreply.github.com>
2022-10-18 12:01:12 +0300

    Удалил 4 папки

--- untitled2/src/GitHub/lessen1/lessen4/HomeWork4.java
+++
@@ -1,7 +0,0 @@
-package GitHub.lessen1.lessen4;
-
-public class HomeWork4 {
-    public static void main(String[] Args){
-        System.out.println("4дз");
-    }
-}
--- untitled2/src/GitHub/lessen2/HomeWork2.java
+++
@@ -1,7 +0,0 @@
-package GitHub.lessen2;
-
-public class HomeWork2 {
-    public static void main(String[] Args){
-        System.out.println("Код по 2му зд");
-    }
-}
